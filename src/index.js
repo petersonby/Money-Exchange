20fae46383c4ae44d50fc3ba5f643e5f4174ec0d
Peterson <contact@peterson.by>
2018-09-28 22:27:11 +0300

    code style refactoring

--- src/index.js
+++ src/index.js
@@ -1,33 +1,33 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-   if (currency > 10000) {
-   	return {error: "You are rich, my friend! We don't have so much coins for exchange"};
-   }
+  if (currency > 10000) {
+    return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+  }
 
-   if (currency < 0) {
-   	return {};
-   }
+  if (currency < 0) {
+    return {};
+  }
 
-   const H = Math.floor(currency / 50);
-   const Q = Math.floor((currency - H * 50) / 25);
-   const D = Math.floor((currency - H * 50 - Q * 25) / 10);
-   const N = Math.floor((currency - H * 50 - Q * 25 - D * 10) / 5);
-   const P = currency - H * 50 - Q * 25 - D * 10 - N * 5;
-   
-   const coins = {
-   	"H": H,
-   	"Q": Q,
-   	"D": D,
-   	"N": N,
-   	"P": P
-   };
+  const H = Math.floor(currency / 50);
+  const Q = Math.floor((currency - H * 50) / 25);
+  const D = Math.floor((currency - H * 50 - Q * 25) / 10);
+  const N = Math.floor((currency - H * 50 - Q * 25 - D * 10) / 5);
+  const P = currency - H * 50 - Q * 25 - D * 10 - N * 5;
+
+  const coins = {
+    "H": H,
+    "Q": Q,
+    "D": D,
+    "N": N,
+    "P": P
+  };
 
 
-   for (key in coins) {
-   	if(coins[key] === 0) {
-   		delete coins[key];
-   	} 
-   }
+  for (key in coins) {
+    if(coins[key] === 0) {
+      delete coins[key];
+    }
+  }
 
-   return coins;
+  return coins;
 }
